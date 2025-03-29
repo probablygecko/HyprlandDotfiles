9d08fcdb8343d2d741bc3d2500455c39328bbf71
Gecko <probablygecko@proton.me>
2025-03-29 14:15:24 +0100

    updated fish

---
+++ fish/conf.d/config.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+    echo (set_color yellow)(date +%T)(set_color normal) on $hostname
+    
+end
---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+# Set prompt colors using femboy pride colors
+set -g fish_color_command ff77aa
+set -g fish_color_keyword dd99ff
+set -g fish_color_param ffccdd
+set -g fish_color_quote bb88ff
+set -g fish_color_redirection ffaad4
+set -g fish_color_comment 555555
+set -g fish_color_error ff77aa
+set -g fish_color_selection_background ffaad4
+set -g fish_color_selection_foreground ffffff
+set -g fish_color_operator d0d0d0
+set -g fish_color_escape ffffff
+set -g fish_color_autosuggestion 888888
+set -g fish_color_user ff77aa
+set -g fish_color_host dd99ff
+set -g fish_color_cwd ffccdd
+set -g fish_color_cwd_root ff0000
+set -g fish_color_valid_path bb88ff
+set -g fish_color_search_match ffaad4
+
